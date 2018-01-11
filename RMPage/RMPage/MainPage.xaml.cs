207806f4e06f8b78c2bb78dda58a38a0a93e2d78
柊 佐藤 <150036@jc-21.jp>
2018-01-11 09:24:49 +0900

    2

--- RMPage/RMPage/MainPage.xaml.cs
+++ RMPage/RMPage/MainPage.xaml.cs
@@ -6,9 +6,13 @@ using System.Linq;
 
 using System.Text;
 
+using System.Threading.Tasks;
+
 
 
 using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
 
 
 
@@ -16,35 +20,211 @@ namespace MuscleTrainingRecords00
 
 {
 
-    public class RMPageCS : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class RMPage : ContentPage
 
     {
 
-        public RMPageCS()
+        public RMPage()
 
         {
 
-            Title = "RM計算";
+            InitializeComponent();
 
-            Content = new StackLayout
+        }
+
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+
+        {
+
+            try
 
             {
 
-                Children = {
+                Double w = Double.Parse(Weight.Text);
 
-                    new Label {
+                Double k = 0.0;
 
-                        Text = "Welcome to RMPage!",
+                String s = (String)count.SelectedItem;
 
-                        HorizontalOptions = LayoutOptions.Center,
+                Double dCount = Double.Parse(s);
 
-                        VerticalOptions = LayoutOptions.CenterAndExpand
+                if (dCount == null)
 
-                    }
+                {
+
+                    DisplayAlert("", "回数を選択してください。", "OK");
+
+                    //RM.Text = "入力してください。";
 
                 }
 
-            };
+
+
+                else if (w == null)
+
+                {
+
+                    DisplayAlert("", "重量を入力してください。", "OK");
+
+                    //Weight.Text = "入力してください。";
+
+                }
+
+
+
+                else if (dCount == 1)
+
+                {
+
+                    k = w / 1;
+
+                    Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 2)
+
+                {
+
+                    k = w / 0.95;
+
+                    Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 3)
+
+                {
+
+                    k = w / 0.93;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 4)
+
+                {
+
+                    k = w / 0.9;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 5)
+
+                {
+
+                    k = w / 0.87;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 6)
+
+                {
+
+                    k = w / 0.85;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 7)
+
+                {
+
+                    k = w / 0.87;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 8)
+
+                {
+
+                    k = w / 0.8;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 9)
+
+                {
+
+                    k = w / 0.77;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+
+
+                else if (dCount == 10)
+
+                {
+
+                    k = w / 0.7;
+
+                    k = Math.Round(k, 1);
+
+                    RM.Text = k.ToString();
+
+                }
+
+            }
+
+
+
+            catch (Exception)
+
+            {
+
+                DisplayAlert("", "入力してください。", "OK");
+
+            }
 
         }
 
